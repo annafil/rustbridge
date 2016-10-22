00711d24c3a9c10b2515f1ca24f7521c7e910995
Anna Filippova <annafilippova@Annas-MacBook-Pro-2.local>
2016-10-22 10:19:31 -0400

    conditionals and more rust style

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-fn add_fifty(n :i32) -> i32 { //can be above or below, works just fine; always have to specify return type as well!
-    n + 50; //on last line of function, if you don't put ; the value of the last line will be returned. if you want to return early using "return" keyword. This is rust style, will throw an error
+fn add_fifty(n :i32) -> i32 { 
+	//can be above or below, works just fine; always have to specify return type as well!
+    n + 50
+    //on last line of function, if you don't put ; the value of the last line will be returned. if you want to return early using "return" keyword. This is rust style, will throw an error
 }
 
 fn main() { //can't have 2 mains
@@ -8,4 +10,15 @@ fn main() { //can't have 2 mains
     age += add_fifty(5); //can't reassign, vars are immutable without mut
     let (number, fruit) = (7, "apples");
     println!("Hi, {}! You are {} years old. You like {} and {}.", name, age, number, fruit);
+
+    let movie_age = 17u32; // unsigned, positive 32bit, rust style to underscore multiple words, snake case warning
+
+    if movie_age < 13 {
+    	println!("You may see G or PG Movies");
+    } else if movie_age < 17 {
+    	println!("You may see G, PG, or PG-13 movies");
+    } else {
+    	println!("You are old.");
+    	println!("You may see G, PG, PG-13, or R movies");
+    }
 }
