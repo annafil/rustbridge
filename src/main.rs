82efdb984c29ee78a28af1d55a0c156c7561faee
Anna Filippova <annafilippova@Annas-MacBook-Pro-2.local>
2016-10-22 10:02:04 -0400

    mutability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+   let name = "Banana";
+   let mut age = 5;
+   age += 5; //can't reassign, vars are immutable without mut
+   let (number, fruit) = (7, "apples");
+   println!("Hi, {}! You are {} years old. You like {} and {}.", name, age, number, fruit);
 }
