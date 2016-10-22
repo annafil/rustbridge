b5942f700fa3cea4337b0c842ba51b6bbe20c081
Anna Filippova <annafilippova@Annas-MacBook-Pro-2.local>
2016-10-22 10:12:48 -0400

    rust style spacing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-fn add_fifty(n :i32) -> i32 {
- n + 50 //on last line of function, if you don't put ; the value of the last line will be returned. if you want to return early using "return" keyword. This is rust style
+fn add_fifty(n :i32) -> i32 { //can be above or below, works just fine; always have to specify return type as well!
+    n + 50; //on last line of function, if you don't put ; the value of the last line will be returned. if you want to return early using "return" keyword. This is rust style, will throw an error
 }
 
-fn main() {
- let name = "Banana";
- let mut age: i32 = 5; // assigns type
- age += add_fifty(5); //can't reassign, vars are immutable without mut
- let (number, fruit) = (7, "apples");
- println!("Hi, {}! You are {} years old. You like {} and {}.", name, age, number, fruit);
+fn main() { //can't have 2 mains
+    let name = "Banana";
+    let mut age: i32 = 5; // assigns type
+    age += add_fifty(5); //can't reassign, vars are immutable without mut
+    let (number, fruit) = (7, "apples");
+    println!("Hi, {}! You are {} years old. You like {} and {}.", name, age, number, fruit);
 }
