fa1f2112aaf7ec546a0d2626cb31d30c69ad3cfc
Anna Filippova <annafilippova@Annas-MacBook-Pro-2.local>
2016-10-22 12:18:10 -0400

    enums and options, refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-fn add_fifty(n :i32) -> i32 { 
-	//can be above or below, works just fine; always have to specify return type as well!
-    n + 50
-    //on last line of function, if you don't put ; the value of the last line will be returned. if you want to return early using "return" keyword. This is rust style, will throw an error
-}
-
 fn main() { //can't have 2 mains
     let name = "Banana";
     let mut age: i32 = 5; // assigns type
@@ -35,42 +29,50 @@ fn main() { //can't have 2 mains
     prices.push(40);
     println!("All prices are: {:#?}", prices); // pretty debug will put on each line
 
-    for i in 0..10 { // exclusive range 
-    	println!("NUmber {}", i);
-    }
+    loops_iterators(age);
 
-    let names = vec!["Carol", "Jake", "Marylou", "Bruce"];
-
-    let name  = "Question"; // this is not the same name as inside the loop 
-
-    for name in names.iter() { // no variable name after the loop
-    	println!("Hi {}!", name); //can access vars outside the for loop
-    	age = add_fifty(age);
-    	println!("{}", age);
-    }
-
-    println!("name {}", name); // prints original name 
-
-    //loop is an infinite loop, while, break exists the current loop
-
-    for i in (0..10).filter(|x| x % 2 == 0) { // keeps what matches
-    	println!("filter = {}", i);
-    }
-
-
-    for i in (0..10).map(|x| x * x) { 
-    	println!("map = {}", i);
-    }
-
-    let sum = (0..10).fold(0, |acc,x| acc + x); // reduce, accumulator, starting at base value pass accumulator value iterating over, value inside block will be returned
-    println!("sum = {}", sum);
-    println!("sum = {}", (0..10).sum::<i32>()); //how does this typing work? 
+    enums();
 
 }
 
 // each part of if condition, if it was in function, would have to return the same type in order to not get errors
 
 // kebab case => movie-age 
+
+fn loops_iterators(age :i32) {
+
+	for i in 0..10 { // exclusive range 
+		println!("NUmber {}", i);
+	}
+
+	let names = vec!["Carol", "Jake", "Marylou", "Bruce"];
+
+	let name  = "Question"; // this is not the same name as inside the loop 
+
+	for name in names.iter() { // no variable name after the loop
+		println!("Hi {}!", name); //can access vars outside the for loop
+		let age = add_fifty(age);
+		println!("{}", age);
+	}
+
+	println!("name {}", name); // prints original name 
+
+	//loop is an infinite loop, while, break exists the current loop
+
+	for i in (0..10).filter(|x| x % 2 == 0) { // keeps what matches
+		println!("filter = {}", i);
+	}
+
+
+	for i in (0..10).map(|x| x * x) { 
+		println!("map = {}", i);
+	}
+
+	let sum = (0..10).fold(0, |acc,x| acc + x); // reduce, accumulator, starting at base value pass accumulator value iterating over, value inside block will be returned
+	println!("sum = {}", sum);
+	println!("sum = {}", (0..10).sum::<i32>()); //how does this typing work? 
+
+}
 
 fn match_function(n :u32) { // no return because not returning things, just printing 
 	match n {
@@ -80,6 +82,69 @@ fn match_function(n :u32) { // no return because not returning things, just prin
 			println!("You are {} years old. You are old", n);
 			println!("You are {} years old. You may see G, PG, PG-13 or R movies", n);
 		},
+	}
+}
+
+fn add_fifty(n :i32) -> i32 { 
+	//can be above or below, works just fine; always have to specify return type as well!
+    n + 50
+    //on last line of function, if you don't put ; the value of the last line will be returned. if you want to return early using "return" keyword. This is rust style, will throw an error
+}
+
+fn enums() {
+	enum TrafficLight {
+		Red,
+		Yellow,
+		Green,
+	}
+
+	let light = TrafficLight::Yellow;
+
+	match light {
+		TrafficLight::Red => println!("STOP!"),
+		//TrafficLight::Yellow => println!("Slow down!"),
+		TrafficLight::Green | TrafficLight :: Yellow => println!("Go go go!"),
+	}
+
+	//enums can hold values! different variants different things 
+
+	enum GameType {
+	    SinglePlayer,
+	    MultiPlayer(u32),
+	}
+
+	let game = GameType::MultiPlayer(10);
+	match game { // curly braces start a new scope 
+	    GameType::SinglePlayer => println!("How about solitaire?"),
+	    GameType::MultiPlayer(2) => println!("How about checkers?"),
+	    GameType::MultiPlayer(4) => println!("How about bridge?"),
+	    GameType::MultiPlayer(num) => {
+	        println!("How about {}-player tag?", num)
+	    },
+	}
+
+	let mut instructors = vec!["Carol"];
+
+	let a = instructors.pop();
+	println!("a is {:?}", a );
+
+	let b = instructors.pop();
+	println!("b is {:?}", b);
+
+	let a = Some("Carol");
+
+	let name = a.expect("No name present");
+	println!("Name is {} bytes long", name.len());
+
+	let b: Option<&str> = None;
+
+	match b {
+		Some(name) => {
+			println!("Other name is {} bytes long", name.len())
+		},
+		None => {
+			println!("No name!")
+		}
 	}
 }
 
